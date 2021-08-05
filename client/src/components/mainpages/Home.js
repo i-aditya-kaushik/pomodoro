0c69401a0e7dfcca5900939fd85c47c5c8733be1
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2021-08-05 16:00:14 +0530

    alert for info aborted due to errors

--- client/src/components/mainpages/Home.js
+++ client/src/components/mainpages/Home.js
@@ -13,28 +13,10 @@ export default function Home(){
     const classes = useStyles();
     const state = useContext(GlobalState);
     const [isLogged] = state.userAPI.isLogged;
-    const [open, setOpen] = React.useState(false);
-    console.log(open,isLogged)
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-          return;
-        }
-        setOpen(false)
-      };
-      useEffect(() => {
-        setTimeout(() => {
-            if(!isLogged){setOpen(true);return}
-        }, 5000);
-      }, []);
     
     return(
         <div>
             <Navbar/>
-            <Snackbar open={open} autoHideDuration={4000} onClose={handleClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
-                <Alert onClose={handleClose} severity="info">
-                    {"Login to Add Tasks and to get sorted in a House!"}
-                </Alert>
-            </Snackbar>
             <Typography>Checking</Typography>
         </div>
     );
