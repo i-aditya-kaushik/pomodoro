5ef518776f07cd8b1ec683b11de58e7b7860cc08
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2021-08-07 09:26:49 +0530

    Customised home for changing dimensions

--- client/src/components/mainpages/Home.js
+++ client/src/components/mainpages/Home.js
@@ -1,5 +1,5 @@
 import Navbar from "../navigation/Navbar";
-import { makeStyles, Paper} from "@material-ui/core";
+import { makeStyles, Paper, useMediaQuery} from "@material-ui/core";
 import React, { useState, useContext,useEffect } from "react";
 import { GlobalState } from "../../GlobalState";
 import Alert from '@material-ui/lab/Alert';
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         resizeMode: 'contain', 
+        minHeight: '91vh'
     }
 }));
 
@@ -22,42 +23,55 @@ export default function Home(){
     const state = useContext(GlobalState);
     const [isLogged] = state.userAPI.isLogged;
     const [house] = state.userAPI.house;
-    var col = "white"
+    const matches = useMediaQuery('(max-width:768px)');
+    var col = "#9c9264"
     var fontcol = "black"
-    var altcol = "#9c9264"
-    var img = "https://res.cloudinary.com/adityakaushik/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1628269706/Hp/hog_banner_inmxc4.png"
+    var altcol = "white"
+    var img = "https://res.cloudinary.com/adityakaushik/image/upload/v1628269706/Hp/hog_banner_inmxc4.png"
     if(house=="Gryffindor"){
         col = "#7f0909"
         fontcol="#ffc500"
         altcol = "#eeba30"
-        img = "https://res.cloudinary.com/adityakaushik/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1628269706/Hp/gry_banner_grwluz.png"
+        img = "https://res.cloudinary.com/adityakaushik/image/upload/v1628269706/Hp/gry_banner_grwluz.png"
     }
     if(house=="Slytherin"){
         col = "#2a623d"
         fontcol="#aaaaaa"
         altcol = "#5d5d5d"
-        img = "https://res.cloudinary.com/adityakaushik/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1628269706/Hp/sly_banner_yvl105.png"
+        img = "https://res.cloudinary.com/adityakaushik/image/upload/v1628269706/Hp/sly_banner_yvl105.png"
     }
     if(house=="Ravenclaw"){
         col="#222f5b"
         fontcol="#946b2d"
         altcol = "#5d5d5d"
-        img = "https://res.cloudinary.com/adityakaushik/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1628269706/Hp/rav_banner_oba4vi.png"
+        img = "https://res.cloudinary.com/adityakaushik/image/upload/v1628269706/Hp/rav_banner_oba4vi.png"
     }
     if(house=="Hufflepuff"){
         col="#fff4b1"
         fontcol="#000000"
         altcol = "#726255"
-        img = "https://res.cloudinary.com/adityakaushik/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1628269706/Hp/huf_banner_j4rbs1.png"
+        img = "https://res.cloudinary.com/adityakaushik/image/upload/v1628269706/Hp/huf_banner_j4rbs1.png"
     }
     return(
         <div>
             <Navbar/>
-            <Grid container component="main" className={classes.root}>
-                <Grid container component={Paper} xs={2} sm={2} md={1} lg={1} xl={1} className={classes.image} style={{backgroundImage:'url('+ img +')', backgroundColor:altcol}}></Grid>
-                <Grid container component={Paper} xs={8} sm={8} md={10} lg={10} xl={10}></Grid>
-                <Grid container component={Paper} xs={2} sm={2} md={1} lg={1} xl={1} className={classes.image} style={{backgroundImage:'url('+ img +')', backgroundColor:altcol}}></Grid>
-            </Grid>
+            
+                {matches ? (
+                    <div>
+                        <Grid container component="main" className={classes.root}>
+                            <Grid container component={Paper} style={{backgroundColor: "#f9f7f5"}}></Grid>
+                        </Grid>
+                    </div>
+                ) : (
+                    <div>
+                        <Grid container component="main" className={classes.root}>
+                            <Grid container component={Paper} xs={2} sm={2} md={1} lg={1} xl={1} className={classes.image} style={{backgroundImage:'url('+ img +')', backgroundColor:col}}></Grid>
+                            <Grid container component={Paper} xs={8} sm={8} md={10} lg={10} xl={10} style={{backgroundColor: "#f9f7f5"}}></Grid>
+                            <Grid container component={Paper} xs={2} sm={2} md={1} lg={1} xl={1} className={classes.image} style={{backgroundImage:'url('+ img +')', backgroundColor:col}}></Grid>
+                        </Grid>
+                    </div>
+                )}
+            
         </div>
     );
 }
