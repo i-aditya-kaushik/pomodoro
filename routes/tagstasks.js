0ffd7f0efd71c96f7e9865b0676f0aef952fc4a8
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2021-08-10 18:29:25 +0530

    tag routes updated

--- routes/tagstasks.js
+++ routes/tagstasks.js
@@ -5,5 +5,7 @@ const tagsController = require("../controller/tagsController");
 router.post("/addtags", auth, tagsController.addtags);
 router.put("/tagupdate", auth, tagsController.tagupdate);
 router.get("/gettags" , tagsController.gettags)
+router.get("/gettagsuser" , auth,  tagsController.gettaguser)
+router.delete("/deletetags" , auth,  tagsController.deletetags)
 
 module.exports = router;
