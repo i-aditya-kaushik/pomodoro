3a1d7e1ace68558bf7f1ef067f7e35d10a9ff291
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2021-08-21 01:25:58 +0530

    backend update

--- controller/taskController.js
+++ controller/taskController.js
@@ -2,18 +2,35 @@ const Tasks = require("../models/taskModel");
 const Users = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Tags = require("../models/tagsModel")
 
 const taskController = {
     addtask: async (req, res) => {
         try {
           const user = await Users.findById(req.user.id);
           if (!user) return res.status(400).json({ msg: "User does not exist."});
-          console.log(req.body)
           const {
             name,
             total_pomodoro,
             tags
           } = req.body;
+          const check = await Tasks.findOne({name: name,total_pomodoro: total_pomodoro})
+          console.log(check)
+          if(check){
+            await Users.findOneAndUpdate(
+            { _id: req.user.id },
+            {
+              $push: {"active_tasks" : {task: check._id}},
+            }
+            );
+            await Tasks.findOneAndUpdate(
+            { _id: check._id },
+            {
+              popularity: check.popularity+1,
+            }
+            );
+            return res.json({ msg: "Added to Tasks list" });
+          }
           const newTask = new Tasks({ name,total_pomodoro,tags});
           const addedtask = await newTask.save();
           req.body.active_tasks = addedtask.id;
@@ -72,12 +89,15 @@ const taskController = {
             pomodoro_done
           } = req.body;
           if(total_pomodoro == pomodoro_done){
-            await Users.findOneAndUpdate(
-              { _id: req.user.id },
-              {
-                $push: {"prev_tasks": id},
-              }
-            );
+            const check = await Users.find({"_id" : req.user.id , prev_tasks : id})
+            console.log(check)
+            if(!check.length)
+              await Users.findOneAndUpdate(
+                { _id: req.user.id },
+                {
+                  $push: {"prev_tasks": id},
+                }
+              );
           }
           await Users.findOneAndUpdate( 
               { _id: req.user.id },
@@ -103,6 +123,24 @@ const taskController = {
             return item
           })
           return res.json({Tasks: final_ret})
+        } catch (err) {
+          return res.status(500).json({ msg: err.message });
+        }
+      },
+      getprevtasks: async (req,res) => {
+        try{
+          const user = await Users.findById(req.user.id);
+          if (!user) return res.status(400).json({ msg: "User does not exist." });
+          const prev_tasks = await Tasks.find( { "_id" : { $in : user.prev_tasks.map(item => {
+            return(item.task)
+          })}}).select("name total_pomodoro _id popularity");
+          const tags = await Users.findById(req.user.id).select("prev_tasks").populate('prev_tasks prev_tasks.tags').sort({ popularity : "descending"})
+          const final_ret = tags.prev_tasks.map(item=>{
+            item = {id:item._id,name:item.name,total_pomodoro:item.total_pomodoro, popularity: item.popularity
+              ,pomodoro_done:item.pomodoro_done}
+            return item
+          })
+          return res.json({final_ret: final_ret})
         } catch (err) {
           return res.status(500).json({ msg: err.message });
         }
--- routes/addtasks.js
+++ routes/addtasks.js
@@ -5,6 +5,7 @@ const taskController = require("../controller/taskController");
 router.post("/addtask", auth, taskController.addtask);
 router.put("/taskupdate", auth, taskController.taskupdate);
 router.get("/gettasksuser", auth, taskController.gettasksuser);
+router.get("/getprevtasks", auth, taskController.getprevtasks);
 router.put("/deleteactivetask", auth, taskController.deleteactivetask);
 router.get("/gettasks" , auth ,taskController.gettasks)
 
